3684b99f89a9795c98d659950b2553a486b78b2b
gbarrigap <gbarrigap@yahoo.es>
2018-02-06 12:48:31 +0000

    use select collections

--- app/models/church.rb
+++ app/models/church.rb
@@ -10,6 +10,13 @@ class Church < ApplicationRecord
   has_many :emails, through: :contacts
   has_many :phones, through: :contacts
 
+  delegate :street_name, to: :address
+  delegate :street_number, to: :address
+
+  delegate :id, to: :commune, prefix: true
+  delegate :id, to: :province, prefix: true
+  delegate :id, to: :region, prefix: true
+
   validates :name, presence: true
   validates :manager, presence: true
 end
