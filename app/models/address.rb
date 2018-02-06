3502a82172da7d7d82ce7975c191425c64224851
Guillermo Barriga Placencia <gbarrigap@yahoo.es>
2018-02-05 21:31:07 -0300

    test uniqueness of street name and number combinations

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,8 +4,7 @@ class Address < ApplicationRecord
   has_one :province, through: :commune
   has_one :region, through: :province
 
-  validates :commune_id, presence: true, uniqueness: true
-  validates :street_name, presence: true, uniqueness: { scope: :commune }
-  validates :street_number, presence: true
+  validates :street_name, presence: true, uniqueness: { scope: [:commune, :street_number] }
+  validates :street_number, presence: true, uniqueness: { scope: [:commune, :street_name] }
   validates :zip_code, uniqueness: { allow_nil: true }
 end
--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -6,6 +6,7 @@ class AddressTest < ActiveSupport::TestCase
     address = addresses(:anunciacion)
     address.commune = nil
     assert_not address.save
+    puts address.errors.first
   end
 
   test 'should not be saved without a street name' do
@@ -30,6 +31,29 @@ class AddressTest < ActiveSupport::TestCase
     address = addresses(:anunciacion)
     address.street_number = ' '
     assert_not address.save
+  end
+
+  test 'should have unique street name and number for a give commune' do
+    foo = addresses(:anunciacion)
+    bar = addresses(:san_ramon)
+    foo.street_name = bar.street_name
+    foo.street_number = bar.street_number
+    assert_not foo.save
+  end
+
+  test 'can have the same street name and number for different communes' do
+    foo = addresses(:anunciacion)
+    bar = addresses(:san_agustin)
+    foo.street_name = bar.street_name
+    foo.street_number = bar.street_number
+    assert foo.save
+  end
+
+  test 'can have the same street name within a given commune, but with a different number' do
+    foo = addresses(:anunciacion)
+    bar = addresses(:san_ramon)
+    foo.street_name = bar.street_name
+    assert foo.save
   end
 
 end
