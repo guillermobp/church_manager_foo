d3698c44c5ec0bacf6d588dc67fafb5aa6bf67b8
Guillermo Barriga Placencia <gbarrigap@yahoo.es>
2018-02-24 23:25:12 -0300

    refactor: use jQuery 'load' function, instead of 'get'

--- app/assets/javascripts/churches.js
+++ app/assets/javascripts/churches.js
@@ -1,40 +1,38 @@
 $(document).on('turbolinks:load', () => {
 
-  $("form").on("ajax:success", (e, data, status, xhr) => {
-    swal("Update", "The data has been updated successfully!", "success");
-  }).on("ajax:error", () => {
-    swal("Update", "An error occured trying to save the data", "error");
+  $('form').on('ajax:success', (e, data, status, xhr) => {
+    swal('Update', 'The data has been updated successfully!', 'success');
+  }).on('ajax:error', () => {
+    swal('Update', 'An error occured trying to save the data', 'error');
   });
 
-  $("select#church_region").on("change", (e) => {
-    var region_id = $(e.target).val();
+  $('select#church_region').on('change', (e) => {
+    var rid = $(e.target).val();
 
-    $("select#church_province").empty();
-
-    $.get(`/regions/${region_id}/provinces`)
-      .then((provinces) => { $("select#church_province").html(provinces) })
-      .always(() => { $("select#church_province").change() });
+    $('select#church_province')
+    .empty()
+    .load(`/regions/${rid}/provinces`, () => {
+      $('select#church_province').change();
+    });
   });
 
-  $("select#church_province").on("change", (e) => {
-    var region_id = $("#church_region").val();
-    var province_id = $(e.target).val();
+  $('select#church_province').on('change', (e) => {
+    var rid = $('#church_region').val();
+    var pid = $(e.target).val();
 
-    $("select#church_commune").empty();
-
-    $.get(`/regions/${region_id}/provinces/${province_id}/communes`)
-      .then((communes) => { $("select#church_commune").html(communes) });
+    $('select#church_commune')
+      .empty()
+      .load(`/regions/${rid}/provinces/${pid}/communes`);
   });
 
-  $("#modal-contact").on("show.bs.modal", (e) => {
-    var id = $(e.relatedTarget).data("id");
-    $.get(`/contacts/${id}`)
-      .then((response) => { $("#modal-contact .modal-body").html(response) });
+  $('#modal-contact').on('show.bs.modal', (e) => {
+    var id = $(e.relatedTarget).data('id');
+    $('#modal-contact .modal-body').load(`/contacts/${id}`);
   });
 
-  $(".modal.priest-details").on("show.bs.modal", (e) => {
-    var id = $(e.relatedTarget).data("id");
-    $.get(`/priests/${id}`).then((response) => { $(".modal.priest-details .modal-body").html(response) })
+  $('.modal.priest-details').on('show.bs.modal', (e) => {
+    var id = $(e.relatedTarget).data('id');
+    $('.modal.priest-details .modal-body').load(`/priests/${id}`);
   });
 
 });
