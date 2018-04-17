656dde4054444b4e1c05702007eeef509b5d0359
Guillermo Barriga Placencia <gbarrigap@yahoo.es>
2018-04-16 22:05:41 -0300

    test: add basic tests for the priest model

--- test/models/priest_test.rb
+++ test/models/priest_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class PriestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should not be saved without a church' do
+    priest = priests(:lovejoy)
+    priest.church = nil
+    assert_not priest.save
+  end
+
+  test 'should not be saved without a first name' do
+    priest = priests(:lovejoy)
+
+    priest.first_name = nil
+    assert_not priest.save
+
+    priest.first_name = ''
+    assert_not priest.save
+  end
+
+  test 'should not be saved without a last name' do
+    priest = priests(:lovejoy)
+
+    priest.last_name = nil
+    assert_not priest.save
+
+    priest.last_name = ''
+    assert_not priest.save
+  end
+
 end
