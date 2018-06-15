346cad008830014a11a836180bd31fe460d8472b
Guillermo Barriga Placencia <gbarrigap@yahoo.es>
2018-06-14 23:36:29 -0400

    create/update people when creating baptisms

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,15 +1,33 @@
 class PeopleController < ApplicationController
 
   def create
-    byebug
+    @person = Person.new(person_params)
+    @selector = params[:selector]
+    @related_selector = params[:related_selector]
+    if @person.save
+      respond_to do |format|
+        format.js
+      end
+    else
+    end
   end
 
   def update
-    byebug
+    @person = Person.find params[:id]
+    @person.update(person_params)
+    @selector = params[:selector]
+    @related_selector = params[:related_selector]
+    if @person.save
+      respond_to do |format|
+        format.js
+      end
+    else
+    end
   end
 
   def nin_form
     @selector = params[:selector]
+    @related_selector = params[:related_selector]
     respond_to do |format|
       format.js
     end
@@ -18,6 +36,7 @@ class PeopleController < ApplicationController
   def person_form_by_nin
     @person = Person.find_or_initialize_by national_identification_number: person_params[:national_identification_number].to_i
     @selector = params[:selector]
+    @related_selector = params[:related_selector]
     respond_to do |format|
       format.js
     end
@@ -26,7 +45,7 @@ class PeopleController < ApplicationController
   private
 
     def person_params
-      params.require(:person).permit(:national_identification_number)
+      params.require(:person).permit(:national_identification_number, :first_name, :last_name, :display_name, :avatar_url, :birth_date, :birth_commune_id)
     end
 
 end
