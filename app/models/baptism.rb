08e876fe6adf1fb2fa0889b1c1652640046ead85
Guillermo Barriga Placencia <gbarrigap@yahoo.es>
2018-06-18 22:40:54 -0400

    baptisms access associated people by role name

--- app/models/baptism.rb
+++ app/models/baptism.rb
@@ -1,5 +1,10 @@
 class Baptism < ApplicationRecord
   belongs_to :priest
+  belongs_to :baptised, class_name: 'Person'
+  belongs_to :mother, class_name: 'Person'
+  belongs_to :father, class_name: 'Person'
+  belongs_to :godmother, class_name: 'Person'
+  belongs_to :godfather, class_name: 'Person'
 
   has_one :church, through: :priest
 end
