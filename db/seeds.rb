ce71cb927f3df65585ad44e8316bfed2f98ab281
Guillermo Barriga Placencia <gbarrigap@yahoo.es>
2018-06-19 22:37:39 -0400

    dev: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,3 +57,30 @@ address = Address.create(street_name: 'Avenida Pedro de Valdivia', street_number
 
 priest = Priest.create(church: church, first_name: 'Agustín', last_name: 'de Hipona', display_name: 'San Agustín', active: true)
 priest = Priest.create(church: church, first_name: 'Osvaldo', last_name: 'Walker', display_name: 'Padre Osvaldo')
+
+# People
+Person.create(national_identification_number: 11111111, first_name: 'Uno', last_name: 'Primero', display_name: 'Uno Primero', avatar_url: nil, birth_date: '1980-01-01', birth_commune_id: Commune.find_by(name: 'Concepción'))
+Person.create(national_identification_number: 22222222, first_name: 'Dos', last_name: 'Segundo', display_name: nil, avatar_url: nil, birth_date: '1980-01-01', birth_commune_id: Commune.find_by(name: 'Concepción'))
+Person.create(national_identification_number: 33333333, first_name: 'Tres', last_name: 'Tercero', display_name: nil, avatar_url: nil, birth_date: '1980-01-01', birth_commune_id: Commune.find_by(name: 'Concepción'))
+Person.create(national_identification_number: 44444444, first_name: 'Cuatro', last_name: 'Cuarto', display_name: nil, avatar_url: nil, birth_date: '1980-01-01', birth_commune_id: Commune.find_by(name: 'Concepción'))
+Person.create(national_identification_number: 55555555, first_name: 'Cinco', last_name: 'Quinto', display_name: nil, avatar_url: nil, birth_date: '1980-01-01', birth_commune_id: Commune.find_by(name: 'Concepción'))
+Person.create(national_identification_number: 66666666, first_name: 'Seis', last_name: 'Sexto', display_name: nil, avatar_url: nil, birth_date: '1980-01-01', birth_commune_id: Commune.find_by(name: 'Concepción'))
+Person.create(national_identification_number: 77777777, first_name: 'Siete', last_name: 'Séptimo', display_name: nil, avatar_url: nil, birth_date: '1980-01-01', birth_commune_id: Commune.find_by(name: 'Concepción'))
+Person.create(national_identification_number: 88888888, first_name: 'Ocho', last_name: 'Octavo', display_name: nil, avatar_url: nil, birth_date: '1980-01-01', birth_commune_id: Commune.find_by(name: 'Concepción'))
+Person.create(national_identification_number: 99999999, first_name: 'Nueve', last_name: 'Noveno', display_name: nil, avatar_url: nil, birth_date: '1980-01-01', birth_commune_id: Commune.find_by(name: 'Concepción'))
+
+# Baptisms
+priest = Priest.find(1)
+baptised = Person.find_by(national_identification_number: 11111111)
+mother = Person.find_by(national_identification_number: 22222222)
+father = Person.find_by(national_identification_number: 33333333)
+godmother = Person.find_by(national_identification_number: 44444444)
+godfather = Person.find_by(national_identification_number: 55555555)
+Baptism.create(priest: priest, baptised: baptised, mother: mother, father: father, godfather: godfather, godmother: godmother, date: Date.current)
+
+baptised = Person.find_by(national_identification_number: 77777777)
+mother = Person.find_by(national_identification_number: 22222222)
+father = Person.find_by(national_identification_number: 33333333)
+godmother = Person.find_by(national_identification_number: 88888888)
+godfather = Person.find_by(national_identification_number: 99999999)
+Baptism.create(priest: priest, baptised: baptised, mother: mother, father: father, godfather: godfather, godmother: godmother, date: Date.current)
