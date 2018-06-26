c4fe2c2f80493a814ae46e1cc8f3156f90ac666c
Guillermo Barriga Placencia <gbarrigap@yahoo.es>
2018-06-25 21:02:32 -0400

    add attribute to display name in case none exists

--- app/models/baptism.rb
+++ app/models/baptism.rb
@@ -7,4 +7,15 @@ class Baptism < ApplicationRecord
   belongs_to :godfather, class_name: 'Person'
 
   has_one :church, through: :priest
+
+  delegate :name_for_display, to: :priest, prefix: true
+  delegate :name_for_display, to: :baptised, prefix: true
+  delegate :name_for_display, to: :mother, prefix: true
+  delegate :name_for_display, to: :father, prefix: true
+  delegate :name_for_display, to: :godmother, prefix: true
+  delegate :name_for_display, to: :godfather, prefix: true
+
+  def date_for_display
+    date.strftime('%B %e, %Y')
+  end
 end
--- app/models/priest.rb
+++ app/models/priest.rb
@@ -8,4 +8,8 @@ class Priest < ApplicationRecord
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def name_for_display
+    display_name || full_name
+  end
 end
