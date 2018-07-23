0d8e361f8ba63d5dbd8bbaec19c4a5ff0ddb773c
Guillermo Barriga Placencia <gbarrigap@yahoo.es>
2018-07-22 22:01:57 -0400

    ui: use DataTable to show registered bastisms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,25 @@
 //= require baptisms
 
 $(document).on('turbolinks:load', () => {
+  let controller = $('body').data('controller');
+  let action = $('body').data('action');
+
   if ($('table').length) {
-    $('table').DataTable();
+    let datatable_options;
+
+    if ('baptisms' === controller && 'index' === action) {
+      datatable_options = {
+        dom: 'ftip',
+        columns: [
+          { 'data': 'row_number' },
+          { 'data': 'name_for_display' },
+          { 'data': 'nin_for_display' },
+          { 'data': 'date' },
+          { 'data': 'actions' }
+        ]
+      };
+    }
+
+    $('table').DataTable(datatable_options);
   }
 });
