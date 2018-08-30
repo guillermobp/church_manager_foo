447e97355810979c9cf3a9c0bbfe0799e06788d0
Guillermo Barriga Placencia <gbarrigap@gmail.com>
2018-08-29 22:19:52 -0300

    get i18n info from url params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_locale
+
   protect_from_forgery with: :exception
 
   def current_church
     @current_church ||= Church.find(1)
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
 end
